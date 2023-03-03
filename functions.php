e1afc6b76c9b75a88159a308ff08d74333762c1d
DaddyMadu <9450141+DaddyMadu@users.noreply.github.com>
2023-03-03 19:22:46 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<link rel="apple-touch-icon" sizes="180x180" href="https://daddymadu.gg/apple-touch-icon.png?v=13">
+<link rel="icon" type="image/png" sizes="32x32" href="https://daddymadu.gg/favicon-32x32.png?v=13">
+<link rel="icon" type="image/png" sizes="16x16" href="https://daddymadu.gg/favicon-16x16.png?v=13">
+<link rel="manifest" href="https://daddymadu.gg/site.webmanifest?v=13">
+<link rel="mask-icon" href="https://daddymadu.gg/safari-pinned-tab.svg?v=13" color="#6c091f">
+<link rel="shortcut icon" href="https://daddymadu.gg/favicon.ico?v=13">
+<meta name="msapplication-TileColor" content="#6c091f">
+<meta name="msapplication-config" content="https://daddymadu.gg/browserconfig.xml?v=13">
+<meta name="theme-color" content="#6c091f">
